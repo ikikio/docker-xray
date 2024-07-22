ecd5bc7f0c2174ae9377dbb8d7eb82f3b501c081
Marshal <marshal@Marshals-MBP.lan>
2024-07-22 15:44:20 +0800

    -

--- build.sh
+++ build.sh
@@ -4,7 +4,4 @@ export DOCKER_BUILDKIT=1
 docker buildx create --name ikik.builder
 docker buildx use ikik.builder
 
-docker buildx build --platform \
-    linux/amd64,linux/arm64,\
-    linux/arm/v7,linux/arm/v8 \
-    -t username/myimage:latest ./
+docker buildx build --platform linux/amd64,linux/arm64 -t ikikio/myimage:latest ./
